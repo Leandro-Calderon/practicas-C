7e4b84632ec79883a7c551cf91be8d982ae4ed03
Leandro-Calderon <mellyleo17@gmail.com>
2025-04-18 05:12:19 -0300

    ejercicios de AUS

---
+++ scanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int alumnos = 0;
+
+  printf("Cuantos alumnos hay?");
+  scanf("%d", &alumnos);
+
+  printf("ok hay %d alumnos\n", alumnos);
+
+  return 0;
+}
