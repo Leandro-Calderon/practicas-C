6c0cbad9a0e190606628ba8ab9f995762c0f1838
Leandro-Calderon <mellyleo17@gmail.com>
2025-04-21 03:38:03 -0300

    nuevos ejercicios

--- salarios.c
+++ salarios.c
@@ -10,6 +10,31 @@
 #include <stdio.h>
 
 int main() {
-    
+    float salario, nuevo_salario;
+
+    printf("Ingrese el salario para saber el aumento correspondiente: ");
+    scanf("%f", &salario); 
+
+    if (salario <= 0) {
+        printf("Error: El salario debe ser un número positivo mayor que cero.\n");
+        return 1;
+    }
+
+    if (salario > 20000) {
+        nuevo_salario = salario * 1.03;
+        printf("Se aplica un aumento del 3%%.\n");
+    } else if (salario >= 15000) {
+        nuevo_salario = salario * 1.05;
+        printf("Se aplica un aumento del 5%%.\n");
+    } else if (salario >= 9000) {
+        nuevo_salario = salario * 1.10;
+        printf("Se aplica un aumento del 10%%.\n");
+    } else {
+        nuevo_salario = salario * 1.20;
+        printf("Se aplica un aumento del 20%%.\n");
+    }
+
+    printf("El salario con aumento es de: %.2f\n", nuevo_salario);
+
     return 0;
 }
