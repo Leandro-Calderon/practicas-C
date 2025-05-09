3e349a2dc4944144aa0bf6e3fd33d8b302b373fc
Leandro-Calderon <mellyleo17@gmail.com>
2025-05-09 19:46:54 -0300

    nuevos ejercicios de C

---
+++ arrays.c
@@ -0,0 +1,19 @@
+// ARRAYS - arreglos - vectores - matrices unidimesionales
+#include <stdio.h>
+#define N 100
+
+int main()
+
+{
+  int notas[N];
+  int i;
+
+  for (i = 0; i < N; i++) {
+    notas[i] + i + 1;
+  }
+
+  for (int i = 0; i < N; i++) {
+    printf("%d ", notas[i]);
+  }
+  return 0;
+}
--- ejercicios/dados.c
+++ ejercicios/dados.c
@@ -1,35 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define N 20
+#include <stdlib.h> //srand & rand
+#include <unistd.h> //getpid
+#define N 1500
 
 int main()
 {
   int dados[N] = {0};
-  int sumas[6] = {0}; // Para contar cuántas veces salió cada número del 1 al 6
+  int sumas[6] = {0}; // sumas[0] 1s    sumas[1] 2s..... sumas[5] 6s
+  int i, j;
+  int max, min;
+  srand(getpid());  
 
-  srand(getpid()); // Semilla para números aleatorios
-  
-  // Tirar el dado N veces
-  for (int i = 0; i < N; i++)
+  for (i = 0; i < N; i++)
   {
-    dados[i] = (rand() % 6) + 1; // Guardar el valor del dado (1 a 6)
-    sumas[dados[i] - 1]++;       // Incrementar el contador correspondiente
-  }
+    dados[i] = (rand() % 6) + 1; // es es un numero entre 1...6
 
-  // Mostrar los valores obtenidos
-  printf("Tiradas:\n");
-  for (int i = 0; i < N; i++)
-  {
+    sumas[dados[i] - 1]++; // a++   a=a+1
+    //  }
+
+    // for(i=0; i<N; i++) {
     printf("%d ", dados[i]);
   }
 
-  // Mostrar cuántas veces salió cada número
-  printf("\n\nFrecuencias:\n");
-  for (int i = 0; i < 6; i++)
+  max = sumas[0]; // sumas[0] guarda cantida de 1s
+  min = N + 1;
+
+  for (i = 0; i < 6; i++)
   {
-    printf("Número %d: %d veces\n", i + 1, sumas[i]);
+    printf("\nAparecio %d veces el numero %d \n", sumas[i], i + 1);
+    for (j = 0; j < sumas[i]; j++)
+    {
+      printf("*");
+    }
+    if (sumas[i] > max)
+      max = sumas[i];
+    if (sumas[i] < min)
+      min = sumas[i];
+  }
+
+  printf("\nLa cantidad maxima de apariciones es %d\n", max);
+  printf("Y los numeros con esa cantidad de apariciones son..");
+  for (i = 0; i < 6; i++)
+  {
+    if (sumas[i] == max)
+      printf("%d ", i + 1);
+  }
+  printf("\nLa cantidad minima de apariciones es %d\n", min);
+  printf("Y los numeros con esa cantidad de apariciones son..");
+  for (i = 0; i < 6; i++)
+  {
+    if (sumas[i] == min)
+      printf("%d ", i + 1);
   }
 
   return 0;
